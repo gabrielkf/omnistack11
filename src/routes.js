709b985ba5f53de97ceb8627213e234bc7870f6b
gabrielkf <gabrielkf@gmail.com>
2020-03-24 23:52:12 -0300

    Added routes: /profile, /session. Added Cors

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,26 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
 const routes = Router();
 
-routes.get("/users", (req, res) => {
-  return res.json({
-    evento: "Omnistack 11",
-    aluno: "Gabriel Paiva"
-  });
-});
+// CONTROLLERS IMPORTS
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+// SESSION
+routes.post('/sessions', SessionController.create);
+
+// ONGs
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+// INCIDENTS
+routes.post('/incidents', IncidentController.store);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+// PROFILE
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
